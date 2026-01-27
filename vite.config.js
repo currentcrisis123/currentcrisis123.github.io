408ca19eb4b77c05cdde9c442642adbebc8faee3
Ethan Loo Yu Teng <eloo6@gatech.edu>
2026-01-27 14:09:22 -0500

    stuff

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/CurrentCrisis/" : "/",
   server: {
     proxy: {
       "/api": {
@@ -26,4 +27,4 @@ export default defineConfig({
   },
   // Copy Unity files to the output directory during build
   publicDir: 'public',
-});
+}));
